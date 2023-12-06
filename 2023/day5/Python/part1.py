a0612677a12abd695c14e5fc3963f778ccf619e4
alibowndyn <63349064+alibowndyn@users.noreply.github.com>
2023-12-06 03:57:27 +0100

    slight optimization

--- 2023/day5/Python/part1.py
+++ 2023/day5/Python/part1.py
@@ -1,7 +1,9 @@
 inp = open('../input.txt', 'r').read()
 
 seeds = list( map(int, inp[7:inp.index('\n')].split(' ')) )
+
 maps = [mapping.splitlines()[1:] for mapping in inp.split('\n\n')]
+maps = [[list(map(int, mm.split(' '))) for mm in m] for m in maps]
 
 locations = list()
 mapped = 0
@@ -12,7 +14,7 @@ for seed in seeds:
 
     for map_ranges in maps:
         for map_range in map_ranges:
-            dst, src, offset = map(int, map_range.split(' '))
+            dst, src, offset = map_range
 
             if src <= mapped <= src + offset:
                 mapped = mapped + (dst - src)
