8254fdb9431606f027867b16c4993f449b3a87d3
alibowndyn <63349064+alibowndyn@users.noreply.github.com>
2023-12-09 22:43:45 +0100

    some simplifications

--- 2023/day9/Python/part1.py
+++ 2023/day9/Python/part1.py
@@ -4,17 +4,12 @@ sum = 0
 
 
 for line in inp:
-    num_sequence = [ *map(int, line.split(' ')) ]
-    sequence_list = []
+    num_sequence = list( map(int, line.split(' ')) )
+    next_num = 0
 
     while any(num_sequence):
-        sequence_list.append(num_sequence)
+        next_num += num_sequence[-1]
         num_sequence = [num_sequence[i] - num_sequence[i-1] for i in range(1, len(num_sequence))]
-
-
-    next_num = 0
-    for i in range(len(sequence_list) - 1, -1, -1):
-        next_num += sequence_list[i][-1]
 
     sum += next_num
 
