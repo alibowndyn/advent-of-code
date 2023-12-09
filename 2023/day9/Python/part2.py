8a9fe9fef31a132a4320022ccbaeb7671a659313
alibowndyn <63349064+alibowndyn@users.noreply.github.com>
2023-12-10 00:43:26 +0100

    another simplification for part 2

--- 2023/day9/Python/part2.py
+++ 2023/day9/Python/part2.py
@@ -12,8 +12,8 @@ for line in inp:
         sequence_list.append(num_sequence)
         num_sequence = [num_sequence[i] - num_sequence[i-1] for i in range(1, len(num_sequence))]
 
-    for i in range(len(sequence_list) - 1, -1, -1):
-        next_num = sequence_list[i][0] - next_num
+    for s in reversed(sequence_list):
+        next_num = s[0] - next_num
 
     sum += next_num
 
