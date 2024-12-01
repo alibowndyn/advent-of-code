a232db79fb4de879f9c392f31fde32dd2d432408
alibowndyn <63349064+alibowndyn@users.noreply.github.com>
2024-12-01 22:53:39 +0100

    Day 1  |  part1 & part2 [C#]

---
+++ 2024/day01/C#/part1_2.cs
@@ -0,0 +1,51 @@
+﻿
+string[] lines = File.ReadAllLines("../input.txt");
+
+List<int> group1 = [];
+List<int> group2 = [];
+
+foreach (string line in lines)
+{
+    string[] parts = line.Split("   ");
+
+    group1.Add( int.Parse(parts[0]) );
+    group2.Add( int.Parse(parts[1]) );
+}
+
+Part1(group1, group2);
+Part2(group1, group2);
+
+
+static void Part1(List<int> group1, List<int> group2)
+{
+    group1.Sort();
+    group2.Sort();
+
+    int total_distance = 0;
+    foreach ((int id_1, int id_2) in group1.Zip(group2))
+    {
+        total_distance += Math.Abs(id_1 - id_2);
+    }
+
+    Console.WriteLine(total_distance);
+}
+
+static void Part2(List<int> group1, List<int> group2)
+{
+    int similarity_score = 0;
+    int count;
+
+    foreach (int id_to_count in group1)
+    {
+        count = 0;
+        foreach (int id in group2)
+        {
+            if (id == id_to_count)
+                count++;
+        }
+
+        similarity_score += id_to_count * count;
+    }
+
+    Console.WriteLine(similarity_score);
+}
